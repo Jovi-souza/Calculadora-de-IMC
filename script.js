49ee30028ae171039ae5cdb76e7446666a0fb52e
João Vitor Rodrigues de souza <jv479439@gmail.com>
2022-05-31 13:45:23 -0300

    correção de bugs completa

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function calcular() {
+    document.getElementById("abaixoDoPeso").classList.add("hiden")
+    document.getElementById("pesoNormal").classList.add("hiden")
+    document.getElementById("acimaDoPeso").classList.add("hiden")
+
     let Weight = document.querySelector('#Weight')
     let height = document.querySelector('#height')
 
@@ -6,59 +10,37 @@ function calcular() {
 
     mensagemDeResultado.innerHTML = `Resultado: ${resultIMC.toFixed(1)}`
 
-    let action = document.getElementById("btnCalcular")
-
-    action.addEventListener("click", function () {
-        let FirstPage = document.getElementById("FirstPage")
-        FirstPage.classList.add("hiden")
-    })
+    let FirstPage = document.getElementById("FirstPage")
+    FirstPage.classList.add("hiden")
 
     if (resultIMC < 18.5) {
         resultIMCAbaixo.innerHTML = 'Abaixo do Peso Normal'
-
-        action.addEventListener("click", function () {
-            let abaixoDoPeso = document.getElementById("abaixoDoPeso")
-            abaixoDoPeso.classList.toggle("hiden")
-        })
+        let abaixoDoPeso = document.getElementById("abaixoDoPeso")
+        abaixoDoPeso.classList.toggle("hiden")
 
     } else if (resultIMC < 25) {
         resultIMCNormal.innerHTML = 'Peso Normal'
-
-        action.addEventListener("click", function () {
-            let pesoNormal = document.getElementById("pesoNormal")
-            pesoNormal.classList.toggle("hiden")
-        })
+        let pesoNormal = document.getElementById("pesoNormal")
+        pesoNormal.classList.toggle("hiden")
 
     } else if (resultIMC < 30) {
         resultIMCAcima.innerHTML = 'Exceso de peso'
-
-        action.addEventListener("click", function () {
-            let acimaDoPeso = document.getElementById("acimaDoPeso")
-            acimaDoPeso.classList.toggle("hiden")
-        })
+        let acimaDoPeso = document.getElementById("acimaDoPeso")
+        acimaDoPeso.classList.toggle("hiden")
 
     } else if (resultIMC < 35) {
         resultIMCAcima.innerHTML = 'Obesidade Classe I'
-
-        action.addEventListener("click", function () {
-            let acimaDoPeso = document.getElementById("acimaDoPeso")
-            acimaDoPeso.classList.toggle("hiden")
-        })
+        let acimaDoPeso = document.getElementById("acimaDoPeso")
+        acimaDoPeso.classList.toggle("hiden")
 
     } else if (resultIMC < 40) {
         resultIMCAcima.innerHTML = 'Obesidade Classe II'
+        let acimaDoPeso = document.getElementById("acimaDoPeso")
+        acimaDoPeso.classList.toggle("hiden")
 
-        action.addEventListener("click", function () {
-            let acimaDoPeso = document.getElementById("acimaDoPeso")
-            acimaDoPeso.classList.toggle("hiden")
-        })
-
-    } else {
+    } else if (resultIMC > 40) {
         resultIMCAcima.innerHTML = 'Obesidade Classe III'
-
-        action.addEventListener("click", function () {
-            let acimaDoPeso = document.getElementById("acimaDoPeso")
-            acimaDoPeso.classList.toggle("hiden")
-        })
+        let acimaDoPeso = document.getElementById("acimaDoPeso")
+        acimaDoPeso.classList.toggle("hiden")
     }
 }
